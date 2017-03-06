5d816bfab8b0a6b25456cf93c2594bc8eaca8767
Runyao Sheng <tiffanyshengus@gmail.com>
2017-03-06 15:15:41 -0800

    tower panel

---
+++ Assets/InventoryTower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryTower : MonoBehaviour {
+
+	public GameObject towerButtonPrefab;
+	public GameObject panelObject;
+	private int p = 260;
+
+	// Use this for initialization
+	void Start () {
+		Sprite t1 =  Resources.Load <Sprite>("cannonIcon");
+		Sprite t2 =  Resources.Load <Sprite>("flameThrowerIcon"); 
+		Sprite t3 =  Resources.Load <Sprite>("teslaIcon"); 
+		Sprite t4 =  Resources.Load <Sprite>("BowlingIcon"); 
+		Sprite[] ts = new Sprite[] {t1, t2, t3, t4 };
+
+		for (int i = 0; i < 4; i++) {
+			GameObject towerButton;
+			towerButtonPrefab.GetComponent<Image>().sprite = ts[i];
+			towerButton = (GameObject) Instantiate(towerButtonPrefab, transform.position, Quaternion.identity);
+			towerButton.transform.parent = panelObject.transform;
+			towerButton.transform.localPosition = new Vector3 (0, p, 0);
+			p -= 50;
+
+		}
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/InventoryTrap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryTrap : MonoBehaviour {
+
+	public GameObject trapButtonPrefab;
+	public GameObject trapButton;
+	public GameObject panelObject;
+
+	// Use this for initialization
+	void Start () {
+		Sprite t1 =  Resources.Load <Sprite>("evilRadio");
+		Sprite[] ts = new Sprite[] {t1};
+		trapButtonPrefab.GetComponent<Image>().sprite = ts[0];
+		trapButton = (GameObject) Instantiate(trapButtonPrefab, transform.position, Quaternion.identity);
+		trapButton.transform.parent = panelObject.transform;
+		trapButton.transform.localPosition = new Vector3 (0, 260, 0);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
